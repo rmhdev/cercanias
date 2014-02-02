2894061db36c5f6248c204e88e51aa08f0f733eb
Rober Martín H <rmh.dev@gmail.com>
2014-02-02 19:34:00 +0100

    Retrieving Route name from html.

--- src/Cercanias/RouteParser.php
+++ src/Cercanias/RouteParser.php
@@ -13,19 +13,29 @@ class RouteParser
         $domDocument = new \DOMDocument("1.0", "utf-8");
         $domDocument->loadHTML($html);
         $path = new \DOMXPath($domDocument);
-        $id = $this->parseStationId($path);
+        $id = $this->parseRouteId($path);
+        $name = $this->parseRouteName($path);
 
-        $this->route = new Route($id, "San Sebastián");
+        $this->route = new Route($id, $name);
         $this->prepareFakeRoute();
         //$this->parseStations($path);
     }
 
-    protected function parseStationId(\DOMXPath $path)
+    protected function parseRouteId(\DOMXPath $path)
     {
         $inputs = $path->query('//input[@name="nucleo"]');
         $value = $inputs->item(0)->attributes->getNamedItem("value")->textContent;
 
         return (int) $value;
+    }
+
+    protected function parseRouteName(\DOMXPath $path)
+    {
+        $titles = $path->query('//title');
+        $title = $titles->item(0)->textContent;
+        $titleParts = explode("  ", $title);
+
+        return utf8_decode($titleParts[1]);
     }
 
 
