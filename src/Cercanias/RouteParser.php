3670196abd76875fdfa4b78132f28392d3f15f5a
Rober Martín H <rmh.dev@gmail.com>
2014-02-02 19:41:35 +0100

    Retrieving stations from html.

--- src/Cercanias/RouteParser.php
+++ src/Cercanias/RouteParser.php
@@ -7,18 +7,27 @@ class RouteParser
 
     protected
         $route;
+
     public function __construct($html)
     {
         libxml_use_internal_errors(true);
         $domDocument = new \DOMDocument("1.0", "utf-8");
         $domDocument->loadHTML($html);
         $path = new \DOMXPath($domDocument);
+        $this->route = $this->createRoute($path);
+    }
+
+    protected function createRoute(\DOMXPath $path)
+    {
         $id = $this->parseRouteId($path);
         $name = $this->parseRouteName($path);
+        $route = new Route($id, $name);
+        $stations = $this->parseStations($path);
+        foreach ($stations as $stationId => $stationName) {
+            $route->addStation(new Station($stationId, $stationName));
+        }
 
-        $this->route = new Route($id, $name);
-        $this->prepareFakeRoute();
-        //$this->parseStations($path);
+        return $route;
     }
 
     protected function parseRouteId(\DOMXPath $path)
@@ -41,13 +50,17 @@ class RouteParser
 
     protected function parseStations(\DOMXPath $path)
     {
+        $stations = array();
         $options = $path->query('//select[@name="o"]/option');
         for ($i = 0; $i < $options->length; $i += 1) {
             $stationName = $options->item($i)->textContent;
             $stationId = $options->item($i)->attributes->getNamedItem("value")->nodeValue;
-            print_r($stationName . ": " . $stationId);
-            print_r("\n");
+            if ($stationId !== "?") {
+                $stations[$stationId] = $stationName;
+            }
         }
+
+        return $stations;
     }
 
     protected function prepareFakeRoute()
