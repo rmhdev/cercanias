1ea6cebfa04d62e14932a7e7aa35f95271e3a7e3
Rober Martín H <rmh.dev@gmail.com>
2014-02-09 13:40:57 +0100

    First attempt to parse a simple timetable.

--- src/Cercanias/TimetableParser.php
+++ src/Cercanias/TimetableParser.php
@@ -10,6 +10,35 @@ class TimetableParser
     public function __construct(Timetable $timetable, $html)
     {
         $this->timetable = $timetable;
+        $this->processHTML($html);
+    }
+
+    protected function processHTML($html)
+    {
+        $previousState = libxml_use_internal_errors(true);
+        $domDocument = new \DOMDocument("1.0", "utf-8");
+        $domDocument->loadHTML($html);
+        $this->updateTimetable(new \DOMXPath($domDocument));
+        libxml_clear_errors();
+        libxml_use_internal_errors($previousState);
+    }
+
+    protected function updateTimetable(\DOMXPath $path)
+    {
+        $allRows = $path->query('//table/tbody/tr');
+        for ($i = 2; $i < $allRows->length; $i += 1) {
+            $tds = $path->query(".//td", $allRows->item($i));
+            $line = $tds->item(0)->textContent;
+            $departureTime = $this->createDateTime($tds->item(1)->textContent);
+            $arrivalTime = $this->createDateTime($tds->item(2)->textContent);
+            $trip = new Trip($line, $departureTime, $arrivalTime);
+            $this->timetable->addTrip($trip);
+        }
+    }
+
+    protected function createDateTime($string)
+    {
+        return new \DateTime();
     }
 
     public function getTimetable()
--- tests/Cercanias/Tests/TimetableParserTest.php
+++ tests/Cercanias/Tests/TimetableParserTest.php
@@ -11,28 +11,37 @@ class TimetableParserTest extends \PHPUnit_Framework_TestCase
 
     public function testGetTimetable()
     {
-        $timetable = new Timetable(
-            new Station(1, "Departure Station"),
-            new Station(2, "Arrival Station")
-        );
-        $parser = new TimetableParser($timetable, "html");
+        $parser = $this->createTimetableParserSanSebastian();
 
         $this->assertInstanceOf('\Cercanias\Timetable', $parser->getTimetable());
     }
 
     public function testGetTimetableCheckBasicData()
     {
-        $parser = new TimetableParser(
+        $parser = $this->createTimetableParserSanSebastian();
+        $timetable = $parser->getTimetable();
+
+        $this->assertEquals("Brincola", $timetable->getDeparture()->getName());
+        $this->assertEquals("Irun", $timetable->getDestination()->getName());
+    }
+
+    protected function createTimetableParserSanSebastian()
+    {
+        return new TimetableParser(
             new Timetable(
                 new Station(123, "Brincola"),
                 new Station(456, "Irun")
             ),
             file_get_contents(__DIR__ . "/../Fixtures/timetable-sansebastian.html")
         );
+    }
+
+    public function testGetTimetableCheckTrips()
+    {
+        $parser = $this->createTimetableParserSanSebastian();
         $timetable = $parser->getTimetable();
 
-        $this->assertEquals("Brincola", $timetable->getDeparture()->getName());
-        $this->assertEquals("Irun", $timetable->getDestination()->getName());
+        $this->assertEquals(20, $timetable->getTrips()->count());
     }
 
 }
