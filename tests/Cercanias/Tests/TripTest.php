69d125b16494808835c5d3ac65bb20a5aa624f4c
Rober Martín H <rmh.dev@gmail.com>
2014-06-14 19:42:06 +0200

    Improve line name in Train.

--- tests/Cercanias/Tests/TripTest.php
+++ tests/Cercanias/Tests/TripTest.php
@@ -17,7 +17,7 @@ class TripTest extends \PHPUnit_Framework_TestCase
 
     protected function createSimpleTrain()
     {
-        return new Train(1, new \DateTime("2014-01-20 12:00:00"), new \DateTime("2014-01-20 12:55:00"));
+        return new Train("c1", new \DateTime("2014-01-20 12:00:00"), new \DateTime("2014-01-20 12:55:00"));
     }
 
     public function testHasTransfer()
