e7817e448fa863db9836702077a93ed1b9df9dd1
Rober Martín H <rmh.dev@gmail.com>
2014-06-21 19:37:09 +0200

    Simplify interface.

--- src/Cercanias/Provider/AbstractProvider.php
+++ src/Cercanias/Provider/AbstractProvider.php
@@ -6,6 +6,19 @@ use Cercanias\HttpAdapter\HttpAdapterInterface;
 
 abstract class AbstractProvider
 {
+    const ROUTE_ASTURIAS = 20;
+    const ROUTE_BARCELONA = 50;
+    const ROUTE_BILBAO = 60;
+    const ROUTE_CADIZ = 31;
+    const ROUTE_MADRID = 10;
+    const ROUTE_MALAGA = 32;
+    const ROUTE_MURCIA_ALICANTE = 41;
+    const ROUTE_SAN_SEBASTIAN = 61;
+    const ROUTE_SANTANDER = 62;
+    const ROUTE_SEVILLA = 30;
+    const ROUTE_VALENCIA = 40;
+    const ROUTE_ZARAGOZA = 70;
+
     protected $httpAdapter;
 
     /**
