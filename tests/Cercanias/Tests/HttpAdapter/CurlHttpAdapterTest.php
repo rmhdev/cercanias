6ee7f0c5b82e0c32f959f9d882329cefa551207f
Rober Martín H <rmh.dev@gmail.com>
2014-06-21 12:48:45 +0200

    Starting with curl tests.

---
+++ tests/Cercanias/Tests/HttpAdapter/CurlHttpAdapterTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Cercanias\Tests\HttpAdapter;
+
+use Cercanias\HttpAdapter\CurlHttpAdapter;
+
+class CurlHttpAdapterTest extends \PHPUnit_Framework_TestCase
+{
+
+    /**
+     * @var CurlHttpAdapter
+     */
+    protected $curl;
+
+    public function setUp()
+    {
+        if (!function_exists('curl_init')) {
+            $this->markTestSkipped('cURL has to be enabled');
+        }
+
+        $this->curl = new CurlHttpAdapter();
+    }
+
+    public function testGetNullContent()
+    {
+        $this->assertNull($this->curl->getContent(null));
+    }
+
+    public function testGetFalseContent()
+    {
+        $this->assertNull($this->curl->getContent(false));
+    }
+
+    public function testGetName()
+    {
+        $this->assertEquals("curl", $this->curl->getName());
+    }
+
+}
