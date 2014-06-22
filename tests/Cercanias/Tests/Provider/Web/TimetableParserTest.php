6ab7f90cf07f1101bbe128e6c6e2caafb889a438
Rober Martín H <rmh.dev@gmail.com>
2014-06-22 18:21:22 +0200

    Improve timetable parser fixtures and tests.

--- tests/Cercanias/Tests/Provider/Web/TimetableParserTest.php
+++ tests/Cercanias/Tests/Provider/Web/TimetableParserTest.php
@@ -86,10 +86,10 @@ class TimetableParserTest extends AbstractTimetableParser
         $timetable = $parser->getTimetable();
         $this->assertEquals(34, $timetable->getTrips()->count());
 
-        $train = new Train("c1", new \DateTime("2014-02-15 22:58"), new \DateTime("2014-02-15 23:10"));
-        $transferTrain = new Train("c3", new \DateTime("2014-02-15 23:37"), new \DateTime("2014-02-16 00:35"));
+        $train = new Train("c1", new \DateTime("2014-06-22 22:58"), new \DateTime("2014-06-22 23:10"));
+        $transferTrain = new Train("c3", new \DateTime("2014-06-22 23:37"), new \DateTime("2014-06-23 00:35"));
         $expectedTrip = new Trip($train, $transferTrain);
-        $trip = $timetable->nextDeparture(new \DateTime("2014-02-15 22:30"));
+        $trip = $timetable->nextDeparture(new \DateTime("2014-06-22 22:30"));
         $this->assertEquals($expectedTrip, $trip);
     }
 
