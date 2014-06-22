480bdd104d8b174a3523f195cf46a0f0880ec88d
Rober Martín H <rmh.dev@gmail.com>
2014-06-22 18:21:39 +0200

    Improve examples.

--- examples/route-buzz.php
+++ examples/route-buzz.php
@@ -3,19 +3,19 @@
 require_once __DIR__ . '/..' . '/vendor/autoload.php';
 
 use Cercanias\Provider\Web\Provider;
-use Cercanias\HttpAdapter\CurlHttpAdapter;
+use Cercanias\HttpAdapter\BuzzHttpAdapter;
 use Cercanias\Station;
 
-$httpAdapter = new CurlHttpAdapter();
+$httpAdapter = new BuzzHttpAdapter();
 $provider = new Provider($httpAdapter);
 
-$route = $provider->getRoute(Provider::ROUTE_SAN_SEBASTIAN);
+$route = $provider->getRoute(Provider::ROUTE_BARCELONA);
 
-echo "Route 'San Sebastián': \n";
+echo "Route 'Barcelona': \n";
 
 foreach ($route->getStations() as $station) {
     /* @var Station $station */
-    echo sprintf(" - [%5d] %s\n", $station->getId(), $station->getName());
+    echo sprintf(" - [%5s] %s\n", $station->getId(), $station->getName());
 }
 
 echo sprintf("results: %d\n", $route->getStations()->count());
---
+++ examples/route-curl.php
@@ -0,0 +1,21 @@
+<?php
+
+require_once __DIR__ . '/..' . '/vendor/autoload.php';
+
+use Cercanias\Provider\Web\Provider;
+use Cercanias\HttpAdapter\CurlHttpAdapter;
+use Cercanias\Station;
+
+$httpAdapter = new CurlHttpAdapter();
+$provider = new Provider($httpAdapter);
+
+$route = $provider->getRoute(Provider::ROUTE_SAN_SEBASTIAN);
+
+echo "Route 'San Sebastián': \n";
+
+foreach ($route->getStations() as $station) {
+    /* @var Station $station */
+    echo sprintf(" - [%5s] %s\n", $station->getId(), $station->getName());
+}
+
+echo sprintf("results: %d\n", $route->getStations()->count());
