6322139a3eaf164af9879279a085b23d78cd9195
Rober MH <rmh.dev@gmail.com>
2018-01-20 20:33:30 +0100

    parse transfer station name

--- src/Cercanias/Provider/HorariosRenfeCom/Parser/TimetableTripsParser.php
+++ src/Cercanias/Provider/HorariosRenfeCom/Parser/TimetableTripsParser.php
@@ -10,8 +10,14 @@
 
 namespace Cercanias\Provider\HorariosRenfeCom\Parser;
 
+use Cercanias\Exception\ParseException;
+
 class TimetableTripsParser
 {
+    const TIMETABLE_WITHOUT_TRANSFER_COLS = 5;
+    const TIMETABLE_WITH_TRANSFER_COLS = 8;
+
+    private $transferStationName;
     private $trips;
 
     public function __construct($html)
@@ -25,27 +31,49 @@ class TimetableTripsParser
         libxml_use_internal_errors($previousState);
     }
 
+    /**
+     * @param \DOMXPath $path
+     * @throws ParseException
+     */
     protected function parse(\DOMXPath $path)
     {
-        $trips = array();
+        $transferStationName = "";
         $allRows = $path->query('//table/tbody/tr');
+        if ($allRows->length <= 0) {
+            throw new ParseException("Timetable has no rows");
+        }
+
+        $headerTds = $allRows->item(0)->getElementsByTagName("td");
+        if (self::TIMETABLE_WITHOUT_TRANSFER_COLS == $headerTds->length) {
+            $trips = $this->parseNoTransferTrips($allRows);
+        } elseif (self::TIMETABLE_WITH_TRANSFER_COLS == $headerTds->length) {
+            $transferStationName = $this->parseTransferStationName($allRows->item(1));
+            $trips = $this->parseTransferTrips($allRows);
+        } else {
+            throw new ParseException(sprintf(
+                'Number of columns in timetable header is unexpected: "%d"',
+                $headerTds->length
+            ));
+        }
+
+
+
+        $this->trips = $trips;
+        $this->transferStationName = $transferStationName;
+    }
+
+    private function parseNoTransferTrips(\DOMNodeList $rows)
+    {
+        $trips = array();
         // The first 2 tr are headers and have no trip data.
-        for ($i = 1; $i < $allRows->length; $i += 1) {
-            $trips[] = $this->parseTrip(
-                $path->query(".//td", $allRows->item($i))
+        for ($i = 1; $i < $rows->length; $i += 1) {
+            $trips[] = $this->parseNoTransferTrip(
+                //$path->query(".//td", $rows->item($i))
+                $rows->item($i)->getElementsByTagName("td")
             );
         }
 
-        $this->trips = $trips;
-    }
-
-    private function parseTrip(\DOMNodeList $list)
-    {
-        if ($this->hasTransfers()) {
-            return array();
-        }
-
-        return $this->parseNoTransferTrip($list);
+        return $trips;
     }
 
     private function parseNoTransferTrip(\DOMNodeList $list)
@@ -67,14 +95,31 @@ class TimetableTripsParser
     }
 
 
-
-    public function hasTransfers()
+    public function hasTransfer()
     {
-        return false;
+        return strlen($this->transferStationName()) > 0;
+    }
+
+    private function parseTransferStationName(\DOMElement $row)
+    {
+        if ($row->getElementsByTagName("td")->length <= 0) {
+            return "";
+        }
+        return trim($row->getElementsByTagName("td")->item(0)->textContent);
+    }
+
+    private function parseTransferTrips(\DOMNodeList $rows)
+    {
+        return array();
     }
 
     public function trips()
     {
         return $this->trips;
+    }
+
+    public function transferStationName()
+    {
+        return $this->transferStationName;
     }
 }
--- tests/Cercanias/Tests/Provider/HorariosRenfeCom/Parser/TimetableTripsParserTest.php
+++ tests/Cercanias/Tests/Provider/HorariosRenfeCom/Parser/TimetableTripsParserTest.php
@@ -21,7 +21,7 @@ class TimetableTripsParserTest extends \PHPUnit_Framework_TestCase
     {
         $parser = new TimetableTripsParser($this->getSimpleTimetableData());
 
-        $this->assertFalse($parser->hasTransfers());
+        $this->assertFalse($parser->hasTransfer());
         $trips = $parser->trips();
         $this->assertEquals(2, count($trips));
 
@@ -36,6 +36,22 @@ class TimetableTripsParserTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals("14:16", $trips[1]["arrival"]);
         $this->assertEquals("0:52", $trips[1]["duration"]);
         $this->assertEquals("CIVIS", $trips[1]["description"]);
+    }
+
+    public function testItParsesTimetableWithTransfer()
+    {
+        $parser = new TimetableTripsParser($this->getTimetableWithTransferData());
+        $this->assertTrue($parser->hasTransfer());
+        $this->assertEquals("Chamartin", $parser->transferStationName());
+    }
+
+    /**
+     * @dataProvider getIncorrectHeaderDataProvider
+     * @expectedException \Cercanias\Exception\ParseException
+     */
+    public function testItThrowsExceptionWhenTimetableHasIncorrectHeader($header, $comments)
+    {
+        new TimetableTripsParser($header);
     }
 
     public function getSimpleTimetableData()
@@ -86,6 +102,191 @@ class TimetableTripsParserTest extends \PHPUnit_Framework_TestCase
 		</tbody>		  
 	</table>
 HTML;
+    }
 
+    public function getTimetableWithTransferData()
+    {
+        return <<<HTML
+<table border="0" width="95%" cellpadding="1" cellspacing="1" align="center" id="tabla" class="horarios">
+		<tbody>
+			 
+		        	<tr>
+			            <td valign="center" class="cabe" align="center" rowspan="3">Línea</td>
+			             <td valign="center" class="cabe" align="center" rowspan="3"></td>  
+			            <td valign="center" class="cabe" align="center" rowspan="3">Salida<br>Origen </td>
+			            <td class="cabe" colspan="2" align="center">Transbordo en</td>
+			            <td valign="center" class="cabe" align="center" rowspan="3">Línea</td>
+			             <td valign="center" class="cabe" align="center" rowspan="3"></td>  
+			            <td valign="center" class="cabe" align="center" rowspan="3">Llegada<br>Destino </td>	           
+			            <td valign="center" class="cabe" align="center" rowspan="3">Tiempo de Viaje</td>
+		          	</tr>
+			        <tr>
+			            <td class="cabe" colspan="2" align="center">Chamartin                               </td>
+			        </tr>
+			        <tr>
+			            <td class="cabe" align="center">Llegada</td>
+			            <td class="cabe" align="center">Salida</td>
+			        </tr>
+	          	
+		          	<tr class="par">
+		            	<td class="linea-cercanias _10C1" align="center" name="codLinea"> 
+		            		 C1    
+		            	</td>
+		            	
+			 			<td align="center">
+			 				<span class="rojo4"> </span>
+			            	
+			            </td>
+			            
+		            	<td class="" align="center"> 05.56 </td>
+						
+			            	<td class="" align="center"> 06.11</td>
+			            	<td class="" align="center"> 06.14 </td>
+			 				
+			 			<td class="linea-cercanias _10C4B" align="center" name="codLinea"> 
+			 				 C4B   
+			 			</td>
+			 			
+			            <td align="center">
+			            <span class="rojo4"> </span>
+			            	
+			            </td>
+			            
+			            <td class="" align="center"> 06.19</td>            
+			            <td class="" align="center"> 0.23 </td>
+		          	</tr>
+	          	
+		          	<tr class="impar">
+		            	<td class="linea-cercanias _10" align="center" name="codLinea"> 
+		            		   
+		            	</td>
+		            	
+			 			<td align="center">
+			 				<span class="rojo4"> </span>
+			            	
+			            </td>
+			            
+		            	<td class="" align="center">  </td>
+						
+			            	<td class="" align="center"> </td>
+			            	<td class="" align="center"> 06.25 </td>
+			 				
+			 			<td class="linea-cercanias _10C4A" align="center" name="codLinea"> 
+			 				 C4A   
+			 			</td>
+			 			
+			            <td align="center">
+			            <span class="rojo4"> </span>
+			            	
+			            </td>
+			            
+			            <td class="" align="center"> 06.30</td>            
+			            <td class="" align="center"> 0.34 </td>
+		          	</tr>
+	          	
+		          	<tr class="par">
+		            	<td class="linea-cercanias _10C10" align="center" name="codLinea"> 
+		            		 C10   
+		            	</td>
+		            	
+			 			<td align="center">
+			 				<span class="rojo4"> </span>
+			            	
+			            </td>
+			            
+		            	<td class="" align="center"> 09.16 </td>
+						
+			            	<td class="" align="center"> 09.31</td>
+			            	<td class="" align="center"> 09.38 </td>
+			 				
+			 			<td class="linea-cercanias _10C4B" align="center" name="codLinea"> 
+			 				 C4B   
+			 			</td>
+			 			
+			            <td align="center">
+			            <span class="rojo4"> </span>
+			            	
+			            </td>
+			            
+			            <td class="" align="center"> 09.43</td>            
+			            <td class="" align="center"> 0.27 </td>
+		          	</tr>
+	          	
+		          	<tr class="impar">
+		            	<td class="linea-cercanias _10C1" align="center" name="codLinea"> 
+		            		 C1    
+		            	</td>
+		            	
+			 			<td align="center">
+			 				<span class="rojo4"> </span>
+			            	
+			            </td>
+			            
+		            	<td class="" align="center"> 09.25 </td>
+						
+			            	<td class="" align="center"> 09.40</td>
+			            	<td class="" align="center"> 09.44 </td>
+			 				
+			 			<td class="linea-cercanias _10C4A" align="center" name="codLinea"> 
+			 				 C4A   
+			 			</td>
+			 			
+			            <td align="center">
+			            <span class="rojo4"> </span>
+			            	
+			            </td>
+			            
+			            <td class="" align="center"> 09.49</td>            
+			            <td class="" align="center"> 0.24 </td>
+		          	</tr>
+	          	
+		          	<tr class="par">
+		            	<td class="linea-cercanias _10" align="center" name="codLinea"> 
+		            		   
+		            	</td>
+		            	
+			 			<td align="center">
+			 				<span class="rojo4"> </span>
+			            	
+			            </td>
+			            
+		            	<td class="" align="center">  </td>
+						
+			            	<td class="" align="center"> </td>
+			            	<td class="" align="center"> 09.52 </td>
+			 				
+			 			<td class="linea-cercanias _10C4B" align="center" name="codLinea"> 
+			 				 C4B   
+			 			</td>
+			 			
+			            <td align="center">
+			            <span class="rojo4"> </span>
+			            	
+			            </td>
+			            
+			            <td class="" align="center"> 09.57</td>            
+			            <td class="" align="center"> 0.32 </td>
+		          	</tr>
+		</tbody>		  
+	</table>
+HTML;
+    }
+
+    public function getIncorrectHeaderDataProvider()
+    {
+        return array(
+            array(
+                '<table><tbody><tr></tr></tbody></table>',
+                'No columns'
+            ),
+            array(
+                '<table><tbody><tr><td></td></tr></tbody></table>',
+                'Few columns'
+            ),
+            array(
+                '<table><tbody><tr><td></td><td></td><td></td><td></td><td></td><td></td></tr></tbody></table>',
+                'Too many columns'
+            ),
+        );
     }
 }
